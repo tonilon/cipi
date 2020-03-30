0d03628ff85d4a3156966189629aa69a56fd2639
Antonio Longo <al.antonio.longo@gmail.com>
2020-03-30 14:21:00 +0200

    Add Level1 main scene
    Add gravity to Player.gd
    Add UP direction to Player.gd

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,9 +3,11 @@ extends KinematicBody2D
 var motion = Vector2(0,0)
 
 const SPEED = 100
+const GRAVITY = 5
+const UP_DIRECTION = Vector2(0,-1)
 
 func _physics_process(delta):
-    
+    apply_gravity()
     if Input.is_action_pressed("right") and not Input.is_action_pressed("left"):
         motion.x = SPEED
     elif Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
@@ -13,4 +15,13 @@ func _physics_process(delta):
     else:
         motion.x = 0
         
-    move_and_slide(motion)
+    move_and_slide(motion, UP_DIRECTION)
+    
+func apply_gravity():
+    if is_on_floor():
+        motion.y = 0
+    else:
+        if motion.y <= 100*GRAVITY:
+            motion.y += GRAVITY
+        else:
+            motion.y = 100*GRAVITY
