1aa483eaf4492a9359b369e5b742901a1ba46ab1
Antonio Longo <al.antonio.longo@gmail.com>
2020-03-30 17:04:31 +0200

    Add jump action and jump code to Player.gd

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,11 +3,14 @@ extends KinematicBody2D
 var motion = Vector2(0,0)
 
 const SPEED = 100
+const JUMP_SPEED = 200
 const GRAVITY = 5
 const UP_DIRECTION = Vector2(0,-1)
 
 func _physics_process(delta):
     apply_gravity()
+    if Input.is_action_just_pressed("jump") and is_on_floor():
+        motion.y -= JUMP_SPEED
     if Input.is_action_pressed("right") and not Input.is_action_pressed("left"):
         motion.x = SPEED
     elif Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
